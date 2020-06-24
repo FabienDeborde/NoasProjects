a0e3b592bf8d43500a3c59ace881e22e96b163ea
Fabien <fabien.webdev@gmail.com>
2020-06-24 17:28:25 +0900

    update heroku support

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/FabienDeborde/noas_projects/database"
 	"github.com/FabienDeborde/noas_projects/project"
@@ -12,15 +13,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/joho/godotenv"
 )
-
-func setupRoutes(app *fiber.App) {
-	app.Get("api/v1/projects", project.GetProjects)
-	app.Get("api/v1/projects/:id", project.GetProject)
-	app.Post("api/v1/projects", project.NewProject)
-	app.Delete("api/v1/projects/:id", project.DeleteProject)
-	app.Post("api/v1/projects/:id/like", project.AddLikeProject)
-	app.Post("api/v1/projects/:id/unlike", project.RemoveLikeProject)
-}
 
 func initDatabase() {
 	var err error
@@ -33,6 +25,16 @@ func initDatabase() {
 	database.DBConn.AutoMigrate(&project.Project{})
 	fmt.Println("Database migrated!")
 
+}
+
+func projectRoutes(group *fiber.Group) {
+	projects := group.Group("/projects") // /api/v1/projects
+	projects.Get("/", project.GetProjects)
+	projects.Post("/", project.NewProject)
+	projects.Get("/:id", project.GetProject)
+	projects.Delete("/:id", project.DeleteProject)
+	projects.Post("/:id/like", project.AddLikeProject)
+	projects.Post("/:id/unlike", project.RemoveLikeProject)
 }
 
 func main() {
@@ -55,17 +57,30 @@ func main() {
 
 	// setupRoutes(app)
 	v1 := app.Group("/api/v1") // /api/v1
-	v1.Get("projects", project.GetProjects)
-	v1.Get("projects/:id", project.GetProject)
-	v1.Post("projects", project.NewProject)
-	v1.Delete("projects/:id", project.DeleteProject)
-	v1.Post("projects/:id/like", project.AddLikeProject)
-	v1.Post("projects/:id/unlike", project.RemoveLikeProject)
+	projectRoutes(v1)
 
 	// 404 Handler
 	app.Use(func(c *fiber.Ctx) {
 		c.SendStatus(404) // => 404 "Not Found"
 	})
 
-	app.Listen(3000)
+	// Log all registered routes
+	for _, r := range app.Routes() {
+		slogger.Infow("Routes",
+			"Method", r.Method,
+			"Path", r.Path,
+		)
+	}
+
+	// Get the PORT from heroku env
+	port := os.Getenv("PORT")
+
+	// Verify if heroku provided the port or not
+	if port == "" {
+		slogger.Warn("Couldn't get the PORT from environment. Switching to default PORT 3000.")
+		port = "3000"
+	}
+
+	// Start server on http://${heroku-url}:${port}
+	app.Listen(port)
 }
